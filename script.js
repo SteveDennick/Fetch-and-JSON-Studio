0ef68ccc1e614516d6afb4b073c58c5d7c5ee52e
Steve Dennick <stdennick@gmail.com>
2021-04-04 14:08:02 -0500

    moved script code

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+      response.json().then(function(json){
+        console.log(json);
+        const astronauts = document.getElementById("container");
+        for (let i = 0; i < json.length; i++){
+          astronauts.innerHTML += `
+          <div class="astronaut">
+            <div class="bio">
+              <h3>${json[i].firstName} ${json[i].lastName}</h3>
+              <ul>
+                <li>Hours in Space: ${json[i].hoursInSpace}</li>
+                <li>Active: ${json[i].active}</li>
+                <li>Skills: ${json[i].skills}</li>
+              </ul>
+            </div>
+            <img class="avatar" src=${json[i].picture}>
+          </div>
+              `;
+        };
+      });
+    });
+  });
